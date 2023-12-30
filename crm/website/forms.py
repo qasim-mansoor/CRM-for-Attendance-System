c253714a3af8d6c37a8a3e137739b39aeeb35fe2
Qasim Ali <qasim.ali1609@gmail.com>
2023-12-30 18:17:11 +0500

    Final Project

---
+++ crm/website/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Customer
+
+class AddRecordForm(forms.ModelForm):
+    customer_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Customer Name", "class":"form-control"}), label="")
+    father_name = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder":"Father Name", "class":"form-control"}), label="")
+    phone_number = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Phone Number", "class":"form-control"}), label="")
+    emergency_number = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder":"Emergency Number", "class":"form-control"}), label="")
+    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Address", "class":"form-control"}), label="")
+    package = forms.CharField(label="", widget=forms.Select(choices=Customer.PACKAGE_CHOICES, attrs={"class":"form-control"}))
+    remarks = forms.CharField(widget=forms.widgets.Textarea(attrs={"placeholder":"Remarks", "class":"form-control"}), label="")
+
+    class Meta:
+        model = Customer
+        exclude = ("user", )
--- crm/website/views.py
+++ crm/website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import Customer
+from .forms import AddRecordForm
 
 # Create your views here.
 def home(request):
+    records = Customer.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -14,12 +17,57 @@ def home(request):
             messages.success(request, "You have been logged in!")
             return redirect('home')
         else:
-            messages.error(request, "There was an error logging in. Please try again.")
+            messages.success(request, "There was an error logging in. Please try again.")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('home')
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Customer.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to view this page.")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Customer.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully.")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to view this page.")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Member Added.")
+                return redirect("home")
+        return render(request, 'add_record.html', {"form": form})
+    else:
+        messages.success(request, "You must be logged in to view this page.")
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Customer.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record has been updated.")
+            return redirect("home")
+        return render(request, 'update_record.html', {"form": form})
+    else:
+        messages.success(request, "You must be logged in to view this page.")
+        return redirect('home')
+        
